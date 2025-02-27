ee0188cff44e858f3e1aadce8987035582c816d2
pukaradhikari1 <157596580+pukaradhikari1@users.noreply.github.com>
2025-02-27 15:46:48 +0545

    bar graph and piechart corrected and other too
    changed

---
+++ EndSemProject-1/README.md
@@ -0,0 +1,1 @@
+"# EndSemProject-1" 
---
+++ EndSemProject-1/mind_your_money/Mind_Your_Money/build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
@@ -0,0 +1,367 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.8.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../../mainwindow.h"
+#include <QtCore/qmetatype.h>
+
+#include <QtCore/qtmochelpers.h>
+
+#include <memory>
+
+
+#include <QtCore/qxptype_traits.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.8.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+QT_WARNING_DISABLE_GCC("-Wuseless-cast")
+namespace {
+struct qt_meta_tag_ZN10MainWindowE_t {};
+} // unnamed namespace
+
+
+#ifdef QT_MOC_HAS_STRINGDATA
+static constexpr auto qt_meta_stringdata_ZN10MainWindowE = QtMocHelpers::stringData(
+    "MainWindow",
+    "updateDateTime",
+    "",
+    "on_btnLogin_clicked",
+    "on_btnSignup_clicked",
+    "on_btnForgot_clicked",
+    "on_btnHome_clicked",
+    "on_btnReset_clicked",
+    "on_btnEnterExpense_clicked",
+    "on_btnSignUpSave_clicked",
+    "on_btnSignUpPrev_clicked",
+    "on_btnPrevExpenseWelcomeUser_clicked",
+    "on_btnSaveExpense_clicked",
+    "on_btnGraph_clicked",
+    "on_btnStats_clicked",
+    "on_btnLogout_clicked",
+    "on_btnChangePassword_clicked",
+    "on_btnNextForgot_clicked",
+    "on_btnPrevForgot_clicked",
+    "on_pushButton_clicked",
+    "on_btnEPush_clicked",
+    "on_btnPrevGraphToWelcome_clicked",
+    "on_btnNext_clicked",
+    "on_Exit0_clicked",
+    "on_Exit1_clicked",
+    "on_Exit2_clicked",
+    "on_Exit3_clicked",
+    "on_Exit4_clicked",
+    "on_Exit5_clicked",
+    "on_Exit6_clicked",
+    "on_Exit7_clicked",
+    "on_Exit8_clicked",
+    "on_Exit9_clicked",
+    "on_btn_LineGraph_clicked",
+    "on_btnLHome_clicked",
+    "on_btn_ExpenseReportHome_clicked",
+    "on_btnSignUpBack_clicked",
+    "on_Exit11_clicked",
+    "on_Exit10_clicked",
+    "on_pushButton_GraphsBarGraph_clicked",
+    "on_pushButton_GraphsLineGraph_clicked",
+    "on_pushButton_GraphsPrev_clicked",
+    "on_pushButton_LineGraphPrev_clicked"
+);
+#else  // !QT_MOC_HAS_STRINGDATA
+#error "qtmochelpers.h not found or too old."
+#endif // !QT_MOC_HAS_STRINGDATA
+
+Q_CONSTINIT static const uint qt_meta_data_ZN10MainWindowE[] = {
+
+ // content:
+      12,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      41,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,  260,    2, 0x08,    1 /* Private */,
+       3,    0,  261,    2, 0x08,    2 /* Private */,
+       4,    0,  262,    2, 0x08,    3 /* Private */,
+       5,    0,  263,    2, 0x08,    4 /* Private */,
+       6,    0,  264,    2, 0x08,    5 /* Private */,
+       7,    0,  265,    2, 0x08,    6 /* Private */,
+       8,    0,  266,    2, 0x08,    7 /* Private */,
+       9,    0,  267,    2, 0x08,    8 /* Private */,
+      10,    0,  268,    2, 0x08,    9 /* Private */,
+      11,    0,  269,    2, 0x08,   10 /* Private */,
+      12,    0,  270,    2, 0x08,   11 /* Private */,
+      13,    0,  271,    2, 0x08,   12 /* Private */,
+      14,    0,  272,    2, 0x08,   13 /* Private */,
+      15,    0,  273,    2, 0x08,   14 /* Private */,
+      16,    0,  274,    2, 0x08,   15 /* Private */,
+      17,    0,  275,    2, 0x08,   16 /* Private */,
+      18,    0,  276,    2, 0x08,   17 /* Private */,
+      19,    0,  277,    2, 0x08,   18 /* Private */,
+      20,    0,  278,    2, 0x08,   19 /* Private */,
+      21,    0,  279,    2, 0x08,   20 /* Private */,
+      22,    0,  280,    2, 0x08,   21 /* Private */,
+      23,    0,  281,    2, 0x08,   22 /* Private */,
+      24,    0,  282,    2, 0x08,   23 /* Private */,
+      25,    0,  283,    2, 0x08,   24 /* Private */,
+      26,    0,  284,    2, 0x08,   25 /* Private */,
+      27,    0,  285,    2, 0x08,   26 /* Private */,
+      28,    0,  286,    2, 0x08,   27 /* Private */,
+      29,    0,  287,    2, 0x08,   28 /* Private */,
+      30,    0,  288,    2, 0x08,   29 /* Private */,
+      31,    0,  289,    2, 0x08,   30 /* Private */,
+      32,    0,  290,    2, 0x08,   31 /* Private */,
+      33,    0,  291,    2, 0x08,   32 /* Private */,
+      34,    0,  292,    2, 0x08,   33 /* Private */,
+      35,    0,  293,    2, 0x08,   34 /* Private */,
+      36,    0,  294,    2, 0x08,   35 /* Private */,
+      37,    0,  295,    2, 0x08,   36 /* Private */,
+      38,    0,  296,    2, 0x08,   37 /* Private */,
+      39,    0,  297,    2, 0x08,   38 /* Private */,
+      40,    0,  298,    2, 0x08,   39 /* Private */,
+      41,    0,  299,    2, 0x08,   40 /* Private */,
+      42,    0,  300,    2, 0x08,   41 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+Q_CONSTINIT const QMetaObject MainWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_ZN10MainWindowE.offsetsAndSizes,
+    qt_meta_data_ZN10MainWindowE,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_tag_ZN10MainWindowE_t,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<MainWindow, std::true_type>,
+        // method 'updateDateTime'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnLogin_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnSignup_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnForgot_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnHome_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnReset_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnEnterExpense_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnSignUpSave_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnSignUpPrev_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnPrevExpenseWelcomeUser_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnSaveExpense_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnGraph_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnStats_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnLogout_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnChangePassword_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnNextForgot_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnPrevForgot_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnEPush_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnPrevGraphToWelcome_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnNext_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_Exit0_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_Exit1_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_Exit2_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_Exit3_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_Exit4_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_Exit5_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_Exit6_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_Exit7_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_Exit8_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_Exit9_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btn_LineGraph_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnLHome_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btn_ExpenseReportHome_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btnSignUpBack_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_Exit11_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_Exit10_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_GraphsBarGraph_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_GraphsLineGraph_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_GraphsPrev_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButton_LineGraphPrev_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
+    >,
+    nullptr
+} };
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    auto *_t = static_cast<MainWindow *>(_o);
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        switch (_id) {
+        case 0: _t->updateDateTime(); break;
+        case 1: _t->on_btnLogin_clicked(); break;
+        case 2: _t->on_btnSignup_clicked(); break;
+        case 3: _t->on_btnForgot_clicked(); break;
+        case 4: _t->on_btnHome_clicked(); break;
+        case 5: _t->on_btnReset_clicked(); break;
+        case 6: _t->on_btnEnterExpense_clicked(); break;
+        case 7: _t->on_btnSignUpSave_clicked(); break;
+        case 8: _t->on_btnSignUpPrev_clicked(); break;
+        case 9: _t->on_btnPrevExpenseWelcomeUser_clicked(); break;
+        case 10: _t->on_btnSaveExpense_clicked(); break;
+        case 11: _t->on_btnGraph_clicked(); break;
+        case 12: _t->on_btnStats_clicked(); break;
+        case 13: _t->on_btnLogout_clicked(); break;
+        case 14: _t->on_btnChangePassword_clicked(); break;
+        case 15: _t->on_btnNextForgot_clicked(); break;
+        case 16: _t->on_btnPrevForgot_clicked(); break;
+        case 17: _t->on_pushButton_clicked(); break;
+        case 18: _t->on_btnEPush_clicked(); break;
+        case 19: _t->on_btnPrevGraphToWelcome_clicked(); break;
+        case 20: _t->on_btnNext_clicked(); break;
+        case 21: _t->on_Exit0_clicked(); break;
+        case 22: _t->on_Exit1_clicked(); break;
+        case 23: _t->on_Exit2_clicked(); break;
+        case 24: _t->on_Exit3_clicked(); break;
+        case 25: _t->on_Exit4_clicked(); break;
+        case 26: _t->on_Exit5_clicked(); break;
+        case 27: _t->on_Exit6_clicked(); break;
+        case 28: _t->on_Exit7_clicked(); break;
+        case 29: _t->on_Exit8_clicked(); break;
+        case 30: _t->on_Exit9_clicked(); break;
+        case 31: _t->on_btn_LineGraph_clicked(); break;
+        case 32: _t->on_btnLHome_clicked(); break;
+        case 33: _t->on_btn_ExpenseReportHome_clicked(); break;
+        case 34: _t->on_btnSignUpBack_clicked(); break;
+        case 35: _t->on_Exit11_clicked(); break;
+        case 36: _t->on_Exit10_clicked(); break;
+        case 37: _t->on_pushButton_GraphsBarGraph_clicked(); break;
+        case 38: _t->on_pushButton_GraphsLineGraph_clicked(); break;
+        case 39: _t->on_pushButton_GraphsPrev_clicked(); break;
+        case 40: _t->on_pushButton_LineGraphPrev_clicked(); break;
+        default: ;
+        }
+    }
+    (void)_a;
+}
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_ZN10MainWindowE.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 41)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 41;
+    }
+    if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 41)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 41;
+    }
+    return _id;
+}
+QT_WARNING_POP
